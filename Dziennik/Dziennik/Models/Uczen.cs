87f8586358202858054d6aa8029f2ec9e493889f
kucharz96 <kucharz96@wp.pl>
2018-11-12 16:32:22 +0100

    uczen

--- Dziennik/Dziennik/Models/Uczen.cs
+++ Dziennik/Dziennik/Models/Uczen.cs
@@ -7,5 +7,20 @@ namespace Dziennik.Models
 {
     public class Uczen
     {
+        public int ID { get; set; }
+        public int IDKlasa { get; set; }
+        public int IDRodzic { get; set; }
+
+        public virtual ICollection<Ocena> Oceny { get; set; }
+        public virtual ICollection<Uwaga> Uwagi { get; set; }
+        public virtual ICollection<Spoznienia> Spoznienia { get; set; }
+        public virtual ICollection<Nieobecnosc> Nieobecnosci { get; set; }
+
+
+        public virtual Rodzic Rodzic { get; set; }
+        public virtual Klasa Klasa { get; set; }
+
+
     }
+
 }
