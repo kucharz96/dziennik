ad71008eb2ad828daf9a021b3be65bba47e029ed
kucharz96 <kucharz96@wp.pl>
2018-11-13 01:28:23 +0100

    entity

---
+++ Dziennik/Dziennik/DAL/Context.cs
@@ -0,0 +1,23 @@
+﻿using Dziennik.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Dziennik.DAL
+{
+    public class Context : DbContext
+    {
+
+        public Context() : base("Context")
+        {
+        }
+
+        public DbSet<Uczen> Uczniowie { get; set; }
+        public DbSet<Przedmiot> Przedmioty { get; set; }
+        
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
